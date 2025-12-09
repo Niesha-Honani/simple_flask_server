877d5f69d26d38143b654524006a518fd7c8befe
VaNiesha Honani <vhonani77@gmail.com>
2025-12-08 19:44:10 -0800

    updated endpoints and put methods in a class

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Module doe endpoints"""
 from flask import Flask, jsonify
-from students import get_student_data, get_old_students, get_younger_students, get_advance_students, get_student_names, get_student_ages
+from students import Students
 
 app =Flask(__name__)
 
@@ -12,38 +12,43 @@ def home():
 @app.route('/api/raw_student_data/', methods=['GET'])
 def api_student_data():
     """Endpoint to retrieve data from student_data function and return as JSON"""
-    data = get_student_data()
+    data = Students.get_student_data()
     return jsonify(data)
 
 @app.route('/api/old_students/', methods=['GET'])
 def api_old_student():
-    student_data = get_student_data()
-    old_students = get_old_students(student_data)
-    return jsonify(old_students)
+    """Method to return old students"""
+    student_data = Students.get_student_data()
+    old_students = Students.get_old_students(student_data)
+    return old_students
 
 @app.route('/api/young_students/', methods=['GET'])
 def api_young_student():
-    student_data = get_student_data()
-    young_students = get_younger_students(student_data)
-    return jsonify(young_students)
+    """Method to return young students"""
+    student_data = Students.get_student_data()
+    young_students = Students.get_younger_students(student_data)
+    return young_students
 
 @app.route('/api/advance_students/', methods=['GET'])
 def advance_students():
-    student_data = get_student_data()
-    advanced_students = get_advance_students(student_data)
-    return jsonify(advanced_students)
+    """Method to return advance students over 21"""
+    student_data = Students.get_student_data()
+    advanced_students = Students.get_advance_students(student_data)
+    return advanced_students
 
 @app.route('/api/student_names/', methods=['GET'])
 def student_names():
-    student_data = get_student_data()
-    student_Names = get_student_names(student_data)
-    return jsonify(student_Names)
+    """Method to return student names"""
+    student_data = Students.get_student_data()
+    student_names_list = Students.get_student_names(student_data)
+    return student_names_list
 
 @app.route('/api/student_ages/', methods=['GET'])
 def student_ages():
-    student_data = get_student_data()
-    ages = get_student_ages(student_data)
-    return jsonify(ages)
+    """Method to return ages"""
+    student_data = Students.get_student_data()
+    ages = Students.get_student_ages(student_data)
+    return ages
 
 
 if __name__ == '__main__':
--- students.py
+++ students.py
@@ -1,55 +1,65 @@
 """Module to hold student data"""
-def get_student_data():
-    """method to return student data"""
-    students = [
-        {'id': '1', 'first_name': 'John', 'last_name': 'Doe', 'age': 18, 'grade': 'A'},
-        {'id': '2', 'first_name': 'Jane', 'last_name': 'Smith', 'age': 19, 'grade': 'B'},
-        {'id': '3', 'first_name': 'Bob', 'last_name': 'Johnson', 'age': 20, 'grade': 'C'},
-        {'id': '4', 'first_name': 'Emily', 'last_name': 'Williams', 'age': 18, 'grade': 'A'},
-        {'id': '5', 'first_name': 'Michael', 'last_name': 'Brown', 'age': 19, 'grade': 'B'},
-        {'id': '6', 'first_name': 'Samantha', 'last_name': 'Davis', 'age': 22, 'grade': 'A'},
-        {'id': '7', 'first_name': 'Oliver', 'last_name': 'Jones', 'age': 20, 'grade': 'B'},
-        {'id': '8', 'first_name': 'Sophia', 'last_name': 'Miller', 'age': 21, 'grade': 'A'},
-        {'id': '9', 'first_name': 'Ethan', 'last_name': 'Wilson', 'age': 19, 'grade': 'C'},
-        {'id': '10', 'first_name': 'Isabella', 'last_name': 'Moore', 'age': 22, 'grade': 'B'}
-    ]
-    return students
+class Students:
+    @staticmethod
+    def get_student_data():
+        """method to return student data"""
+        students = [
+            {'id': '1', 'first_name': 'John', 'last_name': 'Doe', 'age': 18, 'grade': 'A'},
+            {'id': '2', 'first_name': 'Jane', 'last_name': 'Smith', 'age': 19, 'grade': 'B'},
+            {'id': '3', 'first_name': 'Bob', 'last_name': 'Johnson', 'age': 20, 'grade': 'C'},
+            {'id': '4', 'first_name': 'Emily', 'last_name': 'Williams', 'age': 18, 'grade': 'A'},
+            {'id': '5', 'first_name': 'Michael', 'last_name': 'Brown', 'age': 19, 'grade': 'B'},
+            {'id': '6', 'first_name': 'Samantha', 'last_name': 'Davis', 'age': 22, 'grade': 'A'},
+            {'id': '7', 'first_name': 'Oliver', 'last_name': 'Jones', 'age': 20, 'grade': 'B'},
+            {'id': '8', 'first_name': 'Sophia', 'last_name': 'Miller', 'age': 21, 'grade': 'A'},
+            {'id': '9', 'first_name': 'Ethan', 'last_name': 'Wilson', 'age': 19, 'grade': 'C'},
+            {'id': '10', 'first_name': 'Isabella', 'last_name': 'Moore', 'age': 22, 'grade': 'B'}
+        ]
+        return students
 
-def get_old_students(students):
-    older_students_list=[]
-    for student in students:
-        if student['age'] > 20:
-            older_students_list.append(student)
+    @staticmethod
+    def get_old_students(students):
+        """Method to return old students"""
+        older_students_list=[]
+        for student in students:
+            if student['age'] > 20:
+                older_students_list.append(student)
 
-    return older_students_list
+        return older_students_list
 
-def get_younger_students(students):
-    younger_students_list=[]
-    for student in students:
-        if student['age'] < 21:
-            younger_students_list.append(student)
+    @staticmethod
+    def get_younger_students(students):
+        """Method to return new students"""
+        younger_students_list=[]
+        for student in students:
+            if student['age'] < 21:
+                younger_students_list.append(student)
 
-    return younger_students_list
+        return younger_students_list
 
-def get_advance_students(students):
-    advance_students_list = []
-    for student in students:
-        if student['age'] <21 and student['grade'] == 'A':
-            advance_students_list.append(student)
-    return advance_students_list
+    @staticmethod
+    def get_advance_students(students):
+        """Method to return advance students under 21 with a grade A"""
+        advance_students_list = []
+        for student in students:
+            if student['age'] <21 and student['grade'] == 'A':
+                advance_students_list.append(student)
+        return advance_students_list
 
-def get_student_names(students):
-    student_names = []
-    #go through students return firstname.value lastname.value
-    for student in students:
-        student_names.append(student["first_name"] +' '+ student["last_name"])
+    @staticmethod
+    def get_student_names(students):
+        """Method to return students by first and last name"""
+        student_names = []
+        #go through students return firstname.value lastname.value
+        for student in students:
+            student_names.append(student["first_name"] +' '+ student["last_name"])
+        return student_names
 
-    return student_names
-
-def get_student_ages(students):
-    student_ages = []
-    #go through students return firstname.value lastname.value
-    for student in students:
-        student_ages.append(student["first_name"] + ' ' + student["last_name"] + ' ' + str(student["age"]))
-
-    return student_ages
+    @staticmethod
+    def get_student_ages(students):
+        """Method to return student ages"""
+        student_ages = []
+        #go through students return firstname.value lastname.value
+        for student in students:
+            student_ages.append(student["first_name"] + ' ' + student["last_name"] + ' ' + str(student["age"]))
+        return student_ages
